9fb22031fd7a4e451caf832553c469871c8daa70
k <511158469@qq.com>
2019-08-13 14:09:09 +0800

    搜索功能+测试完成

--- gmall-search-web/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
+++ gmall-search-web/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
@@ -1,10 +1,7 @@
 package com.atguigu.gmall.search.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.atguigu.gmall.user.bean.PmsBaseAttrInfo;
-import com.atguigu.gmall.user.bean.PmsSearchParam;
-import com.atguigu.gmall.user.bean.PmsSearchSkuInfo;
-import com.atguigu.gmall.user.bean.PmsSkuAttrValue;
+import com.atguigu.gmall.user.bean.*;
 import com.atguigu.gmall.user.service.AttrService;
 import com.atguigu.gmall.user.service.SearchService;
 import org.apache.commons.lang3.StringUtils;
@@ -12,9 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Controller
 public class SearchController {
@@ -22,13 +17,14 @@ public class SearchController {
     SearchService searchService;
     @Reference
     AttrService attrService;
+
     @RequestMapping("list.html")
-    public String list(PmsSearchParam pmsSearchParam, ModelMap modelMap){// 三级分类id、关键字、
+    public String list(PmsSearchParam pmsSearchParam, ModelMap modelMap) {// 三级分类id、关键字、
 
         // 调用搜索服务，返回搜索结果
-        List<PmsSearchSkuInfo> pmsSearchSkuInfos =  searchService.list(pmsSearchParam);
-        modelMap.put("skuLsInfoList",pmsSearchSkuInfos);
-        Set<String> valueIdSet=new HashSet<>();
+        List<PmsSearchSkuInfo> pmsSearchSkuInfos = searchService.list(pmsSearchParam);
+        modelMap.put("skuLsInfoList", pmsSearchSkuInfos);
+        Set<String> valueIdSet = new HashSet<>();
         for (PmsSearchSkuInfo pmsSearchSkuInfo : pmsSearchSkuInfos) {
             List<PmsSkuAttrValue> skuAttrValueList = pmsSearchSkuInfo.getSkuAttrValueList();
             for (PmsSkuAttrValue pmsSkuAttrValue : skuAttrValueList) {
@@ -39,9 +35,46 @@ public class SearchController {
         // 根据valueId将属性列表查询出来
         List<PmsBaseAttrInfo> pmsBaseAttrInfos = attrService.getAttrValueListByValueId(valueIdSet);
         modelMap.put("attrList", pmsBaseAttrInfos);
-        //返回路径
+        //对平台属性集合进一步处理，去掉当前条件中valueId所在的属性组
+        String[] delValueIds = pmsSearchParam.getValueId();
+        if (delValueIds != null) {
+            //面包屑
+            //delValueIds
+            //pmsSearchParam
+            List<PmsSearchCrumb> pmsSearchCrumbs = new ArrayList<>();
+            for (String delValueId : delValueIds) {
+                //平台属性集合
+                Iterator<PmsBaseAttrInfo> iterator = pmsBaseAttrInfos.iterator();
+                PmsSearchCrumb pmsSearchCrumb = new PmsSearchCrumb();
+                // 生成面包屑的参数
+                pmsSearchCrumb.setValueId(delValueId);
+                pmsSearchCrumb.setUrlParam(getUrlParamForCrumb(pmsSearchParam, delValueId));
+                while (iterator.hasNext()) {
+                    PmsBaseAttrInfo pmsBaseAttrInfo = iterator.next();
+                    List<PmsBaseAttrValue> attrValueList = pmsBaseAttrInfo.getAttrValueList();
+                    for (PmsBaseAttrValue pmsBaseAttrValue : attrValueList) {
+                        String valueId = pmsBaseAttrValue.getId();
+                        if (delValueId.equals(valueId)) {
+                            // 查找面包屑的属性值名称
+                            pmsSearchCrumb.setValueName(pmsBaseAttrValue.getValueName());
+                            //删除该属性值所在的属性组
+                            iterator.remove();
+                        }
+
+                    }
+                }
+                pmsSearchCrumbs.add(pmsSearchCrumb);
+            }
+            modelMap.put("attrValueSelectedList", pmsSearchCrumbs);
+
+        }
+        //获取当前路径的拼接
         String urlParam = getUrlParam(pmsSearchParam);
         modelMap.put("urlParam", urlParam);
+        String keyword = pmsSearchParam.getKeyword();
+        if (StringUtils.isNotBlank(keyword)) {
+            modelMap.put("keyword", keyword);
+        }
 
 
         return "list";
@@ -52,16 +85,13 @@ public class SearchController {
         String catalog3Id = pmsSearchParam.getCatalog3Id();
         String[] skuAttrValueList = pmsSearchParam.getValueId();
         String urlParam = "";
-        if(StringUtils.isNotBlank(keyword))
-        {
+        if (StringUtils.isNotBlank(keyword)) {
             urlParam = urlParam + "keyword=" + keyword;
         }
-        if(StringUtils.isNotBlank(catalog3Id))
-        {
+        if (StringUtils.isNotBlank(catalog3Id)) {
             urlParam = urlParam + "catalog3Id=" + catalog3Id;
         }
-        if(skuAttrValueList!=null)
-        {
+        if (skuAttrValueList != null) {
             for (String SkuAttrValue : skuAttrValueList) {
                 urlParam = urlParam + "&valueId=" + SkuAttrValue;
             }
@@ -71,8 +101,32 @@ public class SearchController {
 
     }
 
+    private String getUrlParamForCrumb(PmsSearchParam pmsSearchParam, String delValueId) {
+        String keyword = pmsSearchParam.getKeyword();
+        String catalog3Id = pmsSearchParam.getCatalog3Id();
+        String[] skuAttrValueList = pmsSearchParam.getValueId();
+        String urlParam = "";
+        if (StringUtils.isNotBlank(keyword)) {
+            urlParam = urlParam + "keyword=" + keyword;
+        }
+        if (StringUtils.isNotBlank(catalog3Id)) {
+            urlParam = urlParam + "catalog3Id=" + catalog3Id;
+        }
+        if (skuAttrValueList != null) {
+            for (String pmsSkuAttrValue : skuAttrValueList) {
+                if (!pmsSkuAttrValue.equals(delValueId)) {
+                    urlParam = urlParam + "&valueId=" + pmsSkuAttrValue;
+                }
+            }
+
+        }
+        return urlParam;
+
+    }
+
+
     @RequestMapping("/index")
-    public String index(){
+    public String index() {
         return "index";
     }
 }
---
+++ gmallapi/src/main/java/com/atguigu/gmall/user/bean/PmsSearchCrumb.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.user.bean;
+
+import lombok.Data;
+
+@Data
+public class PmsSearchCrumb {
+
+    private String valueId;
+    private String valueName;
+    private String urlParam;
+
+
+}
